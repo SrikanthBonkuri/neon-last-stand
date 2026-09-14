b15f9b1172cae07dd48fcd72ce6d312a95db4657
Srikanth B <srikanthbonkuri2730@gmail.com>
2026-09-13 22:41:07 -0500

    Improve aiming and reduce movement speed

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'neon-last-stand-v2';
+const CACHE = 'neon-last-stand-v3';
 const ASSETS = ['./', './index.html', './neon-last-stand.html', './manifest.webmanifest', './neon-last-stand-icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
