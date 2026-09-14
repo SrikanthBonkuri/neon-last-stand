828395eb3d8fb2566c1a406759c46abffa5ebe3d
Srikanth B <srikanthbonkuri2730@gmail.com>
2026-09-13 21:07:49 -0500

    Refresh PWA offline cache

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-const CACHE = 'neon-last-stand-v1';
+const CACHE = 'neon-last-stand-v2';
 const ASSETS = ['./', './index.html', './neon-last-stand.html', './manifest.webmanifest', './neon-last-stand-icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
